71b5ec7aca378ba43880fa221a83928465db78fd
marsxnow <raxelportiz@gmail.com>
2024-11-16 18:38:04 -0800

    Tensorflow working model now working. Create and environment folder or pust pip install tensorflow to system to be able to run env file

--- Node.py
+++ Node.py
@@ -1,20 +1,18 @@
 import numpy as np
+from model import create_model, encode_history
+
 class Node:
-    def __init__(self, info_set):
+    def __init__(self, info_set, model):
         self.info_set = info_set # Information set for node
-        self.regret_sum = np.zeros(2)
-        self.strategy = np.ones(2) / 2 #current strat for every action
+        self.model = model
         self.strategy_sum = np.zeros(2) #sum of all strat so far
 
     def get_strategy(self, realization_weight):
         """ calculate the current mixed strat with regret matching """
-        normalizing_sum = np.sum(np.maximum(self.regret_sum, 0))
-        if normalizing_sum > 0:
-            self.strategy = np.maximum(self.regret_sum, 0) / normalizing_sum
-        else:
-            self.strategy = np.ones(2) / 2 #uniform random strat
-        self.strategy_sum += realization_weight * self.strategy
-        return self.strategy
+        info_arr = encode_history(self.info_set).reshape(1, -1)
+        strategy = self.model.predict(info_arr)[0]
+        self.strategy_sum += realization_weight * strategy
+        return strategy
 
     def get_average_strategy(self):
         """ get average strat across all iterations """

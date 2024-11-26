e81a7213ab607acd8a697e50748b018ec95e30da
marsxnow <raxelportiz@gmail.com>
2024-11-26 15:54:50 -0800

    just needs some comments

--- nodes.py
+++ nodes.py
@@ -1,5 +1,5 @@
 import random
-from typing import List, Dict
+from typing import List, Dict, Optional
 
 Pass = 0
 Bet = 1
@@ -9,13 +9,13 @@ rand = random.Random()
 
 class Node():
     def __init__(self):
-        self.children = {}
+        self.children = ''
         self.strategy = [0.0] * Num_Actions
         self.regretSum = [0.0] * Num_Actions
         self.strategySum = [0.0] * Num_Actions
 
     def __str__(self):
-        return f"Strategy: {self.strategy}, Regret: {self.regretSum}, StrategySum: {self.strategySum}"
+        return self.children + '' + ', '.join(str(x) for x in self.getAvgStrat())
 
     def getStrategy(self, realization_weight: float):
         normalizeSum = 0
@@ -35,3 +35,21 @@ class Node():
         for a in range(Num_Actions):
             avgStrat[a] = self.strategySum[a] / normalizeSum if normalizeSum > 0 else 1.0 / Num_Actions
         return avgStrat
+
+    def returnPayoff(self, cards: List[int]) -> Optional[int]:
+        history = self.children[1:len(self.children)]
+        plays = len(history)
+        currentPlayer = plays % 2
+        opp = 1 - currentPlayer
+
+        if plays > 1:
+            terminalPass = history[plays - 1] == 'p'
+            doubleBet = history[plays - 2] == 'bb'
+            isPlayerCardHigher = cards[currentPlayer] > cards[opp]
+            if terminalPass:
+                if history == 'pp':
+                    return 1 if isPlayerCardHigher else -1
+                else:
+                    return 1
+            elif doubleBet:
+                return 2 if isPlayerCardHigher else -2
